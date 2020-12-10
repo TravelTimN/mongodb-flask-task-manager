02d82e9d70a119c766d8d587593b26b9b1e4b287
Tim Nelson <traveltimn@gmail.com>
2020-12-10 14:58:44 +0000

    Update branch from master to main

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
